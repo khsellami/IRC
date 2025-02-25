a78979b905bfed6eae31713d44549ebd42088684
Khadija Ahmada <kahmada@e3r5p8.1337.ma>
2025-02-25 19:26:26 +0000

    fixed

--- include/Msj.hpp
+++ include/Msj.hpp
@@ -35,47 +35,65 @@ class Msj
 		std::vector<std::string> getArgs(){
 			return this->args;
 		}
-		void SetArgs(const std::string& args) {
-			this->args.clear();
-			this->message.clear();
+void SetArgs(const std::string& args) {
+    this->args.clear();
+    this->message.clear();
 
-			std::string arg;
-			size_t pos = 0;
-			bool messageFound = false;
+    std::string arg;
+    size_t pos = 0;
+    bool messageFound = false;
+    bool hasValidArg = false;  // Vérifie si au moins un argument valide existe
 
-			while (pos < args.length()) {
-				if (args[pos] == ':') {
-					this->message = args.substr(pos + 1);
-					this->message.erase(std::remove(this->message.begin(), this->message.end(), '\n'), this->message.end());
-					this->message.erase(std::remove(this->message.begin(), this->message.end(), '\r'), this->message.end());
+    while (pos < args.length()) {
+        if (args[pos] == ':') {  // Détection du message
+            this->message = args.substr(pos + 1);
 
-					this->args.push_back(this->message);
-					messageFound = true;
-					break;
-				} 
-				else if (args[pos] == ' ') {
-					if (!arg.empty()) {
-						arg.erase(std::remove(arg.begin(), arg.end(), '\n'), arg.end());
-						arg.erase(std::remove(arg.begin(), arg.end(), '\r'), arg.end());
+            // Supprimer les `\n` et `\r`
+            this->message.erase(std::remove(this->message.begin(), this->message.end(), '\n'), this->message.end());
+            this->message.erase(std::remove(this->message.begin(), this->message.end(), '\r'), this->message.end());
 
-						this->args.push_back(arg);
-						arg.clear();
-					}
-				} 
-				else {
-					arg += args[pos];
-				}
-				pos++;
-			}
-			if (!arg.empty() && !messageFound) {
-				arg.erase(std::remove(arg.begin(), arg.end(), '\n'), arg.end());
-				arg.erase(std::remove(arg.begin(), arg.end(), '\r'), arg.end());
-				this->args.push_back(arg);
-			}
-		}
-		std::string get_message(){
-			return this->message;
-		}
+            if (!this->message.empty()) {  
+                this->args.push_back(this->message);
+                hasValidArg = true;
+            }
+            messageFound = true;
+            break;
+        } 
+        else if (args[pos] == ' ') {  
+            if (!arg.empty()) {  // Ajouter seulement si l'argument n'est pas vide ou espace
+                arg.erase(0, arg.find_first_not_of(" \n\r")); // Supprime les espaces au début
+                arg.erase(arg.find_last_not_of(" \n\r") + 1); // Supprime les espaces à la fin
+
+                if (!arg.empty()) {  // Vérifie si l'argument n'est pas vide après nettoyage
+                    this->args.push_back(arg);
+                    hasValidArg = true;
+                }
+                arg.clear();
+            }
+        } 
+        else {
+            arg += args[pos];
+        }
+        pos++;
+    }
+
+    // Ajouter le dernier argument s'il est valide
+    if (!arg.empty() && !messageFound) {
+        arg.erase(0, arg.find_first_not_of(" \n\r"));
+        arg.erase(arg.find_last_not_of(" \n\r") + 1);
+
+        if (!arg.empty()) {
+            this->args.push_back(arg);
+            hasValidArg = true;
+        }
+    }
+
+    // Si aucun argument valide n'a été ajouté, stocker "NULL"
+    if (!hasValidArg) {
+        this->args.push_back("NULL");
+    }
+}
+
 };
 
 #endif
